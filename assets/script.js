cdc45d156fe3b92eafdc6c144a8426727a1d235f
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2021-11-04 14:23:32 -0500

    Added variables with character strings for password

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,50 @@
 // Assignment code here
 
-
 // Get references to the #generate element
+Math.floor(8,128)
+Math.ceil(8,128)
+
 var generateBtn = document.querySelector("#generate");
+
+var passwordLowerCaseLetters = "abcdefghisklmnopqrstuvwxyz"
+
+var passwordUpperCaseLetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var passwordSpecialCharacters = "!@#$%^&*()-_=+[]{}/?`~.*"
+
+var passwordNumbers = "123456789"
+
+var passwordlength = window.prompt(i)
+
+function generatePassword(){
+
+
+ console.log("button clicked");
+
+
+// 1. prompt the user password criteria 
+window.prompt("enter length of password")
+
+ 
+
+  //length 8-128- set ceiling/floor 
+ 
+  //uppercase,lowercase,numbers,special characters, make an index?
+
+// 2. validate the input
+
+// 3. Generate password based on criteria
+
+
+
+
+
+
+// 4. Display password on page
+
+return "generated password";
+
+}
 
 // Write password to the #password input
 function writePassword() {
