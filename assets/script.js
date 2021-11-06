33869b497cda3acb405a50c278d32a2859cfb457
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2021-11-06 18:54:23 -0500

    added password type confirmations
    changed passLength to passSize for clarity
    
    bugs: Clicking the generate password button is now broken.

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,17 @@ var generateBtn = document.querySelector("#generate");
 //variables
 //variables for key selection
 
-var passwordLowerCase = ["'a','b','c','d','e','f','g','h','i','s','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'"]
+var passwordLowerCase = ["'a','b','c','d','e','f','g','h','i','s','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'"];
 
-var passwordUpperCase = ["'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'"]
+var passwordUpperCase = ["'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'"];
 
-var passwordSpecialCharacters = ["'!','@','#','$','%',';','^','&','*','(',')','-','_','=','+','[',']','{','}','/','?','`','~','.','*'"]
+var passwordSpecialCharacters = ["'!','@','#','$','%',';','^','&','*','(',')','-','_','=','+','[',']','{','}','/','?','`','~','.','*'"];
 
-var passwordNumbers = ["'1','2','3','4','5','6','7','8','9'"]
+var passwordNumbers = ["'1','2','3','4','5','6','7','8','9'"];
+
+var i = 0
+
+
 // vars will need to go through catcatination and then use the math.random()
 
 //end of key selection
@@ -28,61 +32,57 @@ function generatePassword() {
   console.log("button clicked");
 
 
-  // 1. prompt the user password criteria 
+  // 1. prompt the user password for password length
 
-  passLength = prompt("Enter a number value between 8-128.");
- if (passLength === null) {
-   return;
- }
+  passSize = prompt("Enter a number value between 8-128.");
+  if (passSize === null) {
+    return;
+  }
 
-//if entry is anything besides a # between 6-128 alert is prompted  
-if (passLength < 8 || passLength >128 || isNaN(passLength)) {  
-alert("ERROR: Entry must be a number between 8 and 128. ")
-return;
+  //if entry is anything besides a # between 6-128 alert is prompted  
+  if (passSize < 8 || passSize > 128 || isNaN(passSize)) {
+    alert("ERROR: Entry must be a number between 8 and 128. ")
+    return;
 
+  }
+
+  alert("You have chosen your password to be " + passSize + " characters long.")
+
+
+  var passLow = confirm("Include lower-case letters in your password?");
+
+  var passUpp = confirm("Include upper-case numbers in your password?");
+
+  var passSpec = confirm("Include special characters in your password?");
+
+  var passNum = confirm("Include Numbers in your password?");
+
+  passArray = "";
+
+  if (passNum === true) {
+    passArray = passArray.concat(passNumbers);
+  }
+
+  if (passSpec === true) {
+    passArray = passArray.concat(passwordSpecialCharacters);
+  }
+
+  if (passUpp === true) {
+    passArray = passArray.concat(passwordUpperCase);
+  }
+
+  if (passLow === true) {
+    passArray = PassArray.concat(passwordLowerCase);
+  }
+
+  for (i - 0; i < passSize; i++)
+  {
+  var randomPassword = [Math.floor(Math.random)() * passArray.length];
+  }
+ 
 }
 
-alert("You have chosen your password to be " + passLength + " characters long.")
-
-
-var passLow = confirm("Include lower-case letters in your password?");
-
-var passUpp = confirm("Include upper-case numbers in your password?")
-
-var passSpec = confirm("Include special characters in your password?")
-
-var passNum = confirm("Include Numbers in your password?");
-
-
-if (passwordNum === true){
-passArray = passArray.concat(passNumbers);
-}
-
-if (passSpec === true){
-  passArray = passArray.concat(passwordSpecialCharacters);
-}
-
-if (passUpp === true){
-  passArray = passArray.concat(passwordUpperCase);
-}
-
-if (passLow ===true){
-  passArray = PassArray.concat(passwordLowerCase);
-}
-  //length 8-128- set ceiling/floor 
-
-  //uppercase,lowercase,numbers,special characters, make an index?
-
-  // 2. validate the input
-
-  // 3. Generate password based on criteria
-
-  // 4. Display password on page
-
-  passArray = []
-  return "generated password";
-
-}
+return "generated password";
 
 // Write password to the #password input
 function writePassword() {
@@ -92,6 +92,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+console.log
+//return array
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
