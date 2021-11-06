54a1fc2ba0283483a7ac7318cdc758e7d7e0ed4a
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2021-11-06 10:17:47 -0500

    Completed password length option

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,63 @@
 // Assignment code here
 
 // Get references to the #generate element
-Math.floor(8,128)
-Math.ceil(8,128)
+Math.floor(8, 128)
 
 var generateBtn = document.querySelector("#generate");
 
-var passwordLowerCaseLetters = "abcdefghisklmnopqrstuvwxyz"
+//variables
+//variables for key selection
 
-var passwordUpperCaseLetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var passwordLowerCase = "abcdefghisklmnopqrstuvwxyz"
+
+var passwordUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 var passwordSpecialCharacters = "!@#$%^&*()-_=+[]{}/?`~.*"
 
 var passwordNumbers = "123456789"
+// vars will need to go through catcatination and then use the math.random()
 
-var passwordlength = window.prompt(i)
+//end of key selection
 
-function generatePassword(){
-
-
- console.log("button clicked");
+//variables for prompts
 
 
-// 1. prompt the user password criteria 
-window.prompt("enter length of password")
 
- 
+//end of prompt variables
+// end of variables
 
+
+
+function generatePassword() {
+
+  console.log("button clicked");
+
+
+  // 1. prompt the user password criteria 
+
+  passLength = prompt("Enter a number value between 8-128.");
+ if (passLength === null) {
+   return;
+ }
+
+//if entry is anything besides a # between 6-128 alert is prompted  
+if (passLength < 8 || passLength >128 || isNaN(passLength)) {  
+alert("ERROR: Entry must be a number between 8 and 128. ")
+
+}
+
+alert("You have chosen your password to be " + passLength + " characters long.")
   //length 8-128- set ceiling/floor 
- 
+
   //uppercase,lowercase,numbers,special characters, make an index?
 
-// 2. validate the input
+  // 2. validate the input
 
-// 3. Generate password based on criteria
+  // 3. Generate password based on criteria
 
+  // 4. Display password on page
 
-
-
-
-
-// 4. Display password on page
-
-return "generated password";
+  return "generated password";
 
 }
 
